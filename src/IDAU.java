1c7991e2457b9f1c6765d1b841605d32cb3f8174
etushr@gmail.com <etushr@gmail.com>
2022-01-24 02:08:13 +0200

    Here is my commit message

--- src/IDAU.java
+++
@@ -1,9 +0,0 @@
-import java.util.LinkedHashSet;
-import java.util.Map;
-
-interface IDAU {
-
-    Map<String, LinkedHashSet<String>> dailyUsers();
-
-    int dailyActiveUsers(String date, Map<String, LinkedHashSet<String>> map);
-}
---
+++ src/test/java/DailyActiveUsersTest.java
@@ -0,0 +1,14 @@
+/*
+import static org.junit.Assert.*;
+
+public class DailyActiveUsersTest {
+
+    @org.junit.Test
+    public void dailyUsers() {
+
+    }
+
+    @org.junit.Test
+    public void dailyActiveUsers() {
+    }
+}*/
